b88da768074e7a15f182de82c4a33f32baf92d18
luoway <luoway@outlook.com>
2023-08-21 00:39:11 +0800

    init node ssr

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<main>
+    <div>count: {count}</div>
+    <button on:click={counter}>add</button>
+</main>
+<script lang="ts">
+    export let count = 0
+    export function counter(e: Event){
+        count += 1
+        console.log('#9')
+    }
+</script>
