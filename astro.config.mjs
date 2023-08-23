708798773537b2a9b522bf94d33f46ef170ab74c
luoway <luoway@outlook.com>
2023-08-23 15:22:42 +0800

    init ssr

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config'
+import node from '@astrojs/node'
+import vue from '@astrojs/vue'
+
+// https://astro.build/config
+export default defineConfig({
+    output: 'server',
+    adapter: node({
+        mode: 'standalone',
+    }),
+    integrations: [vue()],
+})
---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>{{ count }}</div>
+    <button @click="add">add</button>
+</template>
+
+<script>
+export default {
+    name: 'DemoSsrFrameworksCounter',
+
+    data() {
+        return {
+            count: 0
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        add(){
+            this.count ++
+        }
+    },
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
